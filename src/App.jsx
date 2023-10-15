5ffa45a2379d83825969d6636c80754f73078cd4
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2023-10-15 13:01:26 +0500

    Ready for development

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello Word! I am Notes App</h1>
+    </div>
+      )
+}
+
+export default App
